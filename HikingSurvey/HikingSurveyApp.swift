26f82b122f54f65eaa00e2d5cd2701be8dca5e49
Fabiana Petrovick <fafadiaz@hotmail.com>
2025-06-24 09:28:02 -0300

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Macbook on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
